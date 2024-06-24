ef372ff2aeda99c897f4eb0d6504a9356abafa5d
otsha <otso.haavisto@aalto.fi>
2024-06-24 15:24:07 +0300

    Part 4: group exercise instructions + skeleton

---
+++ exercises/E_Streaming-between-devices/marker_gen.py
@@ -0,0 +1,22 @@
+from pylsl import StreamInfo, StreamOutlet
+import time
+import random
+
+def create_outlet():
+    # TODO: Define StreamInfo below
+
+    stream = StreamInfo(name, type, n_of_channels, sampling_rate, value_type, outlet_id)    # Establish the details of the stream
+    outlet = StreamOutlet(stream)                                                           # Create the outlet
+
+    return outlet
+
+def output_marker(marker_text, target_outlet):                                              # Take the marker as a parameter
+    print('Outputting random sample:', marker_text)                                         # Print out the marker
+
+    target_outlet.push_sample([marker_text])                                                # Push the marker through the outlet
+
+outlet = create_outlet()
+
+while True:
+    output_marker(marker_text = 'heyo', target_outlet = outlet)
+    time.sleep(1)
